35b83d960f1b7a10cfe4c48bfbdb37e6d7ffb282
Cr4mlin <Cramlin@yandex.ru>
2026-04-25 14:55:10 +0700

    urls

---
+++ apps/users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from users import views
+
+urlpatterns = [
+    path('accounts/register/', views.RegisterView.as_view(), name='register'),
+    path('accounts/login/', views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', views.LogoutView.as_view(), name='logout'),
+    path('accounts/verify-email/<uidb64>/<token>/', views.VerifyEmailView.as_view(), name='verify_email'),
+]
