8a0e093a7a5d731096f0606139e3b5ba6f8d9751
m4getsu <kostya.kulikov20051111@gmail.com>
2026-04-26 04:07:32 +0700

    update profile/edit page (fix avatars (legenda ob aange))

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
+from PIL import Image
+
+AVATAR_SIZE = (200, 200)
 
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    avatar = models.ImageField(upload_to='media/avatars/', blank=True, null=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
     birth_date = models.DateField(blank=True, null=True)
     steam_id = models.CharField(max_length=100, blank=True, null=True)
@@ -16,6 +19,15 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'Профиль {self.user.username}'
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        if self.avatar:
+            img = Image.open(self.avatar.path)
+            if img.size != AVATAR_SIZE:
+                img = img.resize(AVATAR_SIZE, Image.LANCZOS)
+                img.save(self.avatar.path)
+            img.close()
 
 
 class User(AbstractUser):
--- project/urls.py
+++ project/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
+from django.conf import settings
+from django.conf.urls.static import static
 
 def home(request):
     return HttpResponse('Главная — в разработке')
@@ -25,4 +27,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
     path('', include('users.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
