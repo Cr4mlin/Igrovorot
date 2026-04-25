54b543e2c89eef6d61792928b40eb69bfb67fbeb
Cr4mlin <Cramlin@yandex.ru>
2026-04-25 14:42:21 +0700

    form

---
+++ apps/users/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from users.models import User
+
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    username = forms.CharField(required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
+        user.is_active = False
+        if commit:
+            user.save()
+        return user
