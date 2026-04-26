d6f9fe8d910f92a3dd47b09cc4c82b1cab556a3c
m4getsu <kostya.kulikov20051111@gmail.com>
2026-04-26 23:05:14 +0700

    main page posts (without social part)

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -14,3 +14,15 @@ class PostListView(View):
         page_number = request.GET.get('page')
         page_obj = paginator.get_page(page_number)
         return render(request, self.template_name, {'page_obj': page_obj})
+
+
+class FeedView(View):
+    template_name = 'posts/feed.html'
+    paginate_by = 10
+
+    def get(self, request):
+        posts = Post.objects.filter(is_published=True).order_by('-created_at')
+        paginator = Paginator(posts, self.paginate_by)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        return render(request, self.template_name, {'page_obj': page_obj})
