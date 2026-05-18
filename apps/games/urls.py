e83a5fdb5746997a3596e4e7efd646a4024ac54c
m4getsu <kostya.kulikov20051111@gmail.com>
2026-05-19 03:45:22 +0700

    cache (not mirage)

--- apps/games/urls.py
+++ apps/games/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 from games import views
 
 urlpatterns = [
-    path('games/', views.GameListView.as_view(), name='games'),
+    path('games/', cache_page(60 * 15)(views.GameListView.as_view()), name='games'),
     path('games/<slug:slug>/', views.GameDetailView.as_view(), name='game_detail'),
     path('games/<slug:slug>/delete/', views.GameDeleteView.as_view(), name='game_delete'),
 ]
